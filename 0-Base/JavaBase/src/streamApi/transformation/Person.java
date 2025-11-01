ee4c5b78008f57ca106302af99c8721c274a1f38
maerdlina <maerdlina@gmail.com>
2025-11-01 12:01:02 +0300

    stream api

---
+++ 0-Base/JavaBase/src/streamApi/transformation/Person.java
@@ -0,0 +1,19 @@
+package streamApi.transformation;
+
+public class Person {
+    private String name;
+    private int age;
+    private String city;
+    private String car;
+
+    public Person(String name, int age, String city, String car) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
